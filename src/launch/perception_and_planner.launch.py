228de4111a7232707bd30ac8c2cb677d55224241
PopeMattA <mpope@ucsd.edu>
2025-05-27 18:30:28 -0700

    adding perception and task planning node

---
+++ src/launch/perception_and_planner.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ros2_aruco_perception',
+            executable='perception_node',
+            name='aruco_perception_node',
+            output='screen',
+        ),
+        Node(
+            package='ros2_task_planner',
+            executable='task_planner_node',
+            name='task_planner_node',
+            output='screen',
+        ),
+    ])
