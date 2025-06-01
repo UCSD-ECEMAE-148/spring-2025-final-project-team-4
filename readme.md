517b8132f7f32c884fe66826d5e2ed8a2f4afe0b
PopeMattA <mpope@ucsd.edu>
2025-05-31 19:53:47 -0700

    add readme launch instruction and usbcam launches

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # spring-2025-final-project-team-4
+
+ros2 launch ros2_aruco_perception perception_and_planner.launch.py
--- src/launch/perception_and_planner.launch.py
+++ src/launch/perception_and_planner.launch.py
@@ -4,6 +4,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
+            package='usb_cam_driver',
+            executable='usb_cam_node',
+            name='usb_cam_node',
+            output='screen',
+        ),
+        Node(
             package='ros2_aruco_perception',
             executable='perception_node',
             name='aruco_perception_node',
